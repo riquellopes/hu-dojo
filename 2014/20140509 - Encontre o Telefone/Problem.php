4309e5479ee6c94ef5038215983cca6c07e68bbd
Vitor Mattos <vitor.sozua@hotelurbano.com>
2014-05-09 17:17:33 -0300

    glauco foeppel e Raul Jr

--- 2014/20140509 - Encontre o Telefone/Problem.php
+++ 2014/20140509 - Encontre o Telefone/Problem.php
@@ -5,9 +5,9 @@ class Teclado {
         if ($letra == 'D' || $letra == 'E' || $letra == 'F')
             return 3;
         else if ($letra == 'A' || $letra == 'B' || $letra == 'C')
-        
+            return 2;
+        else            
             return $letra;
-        return 2;
     }
     
 }
