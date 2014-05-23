e59cdc0cec5eca875087c5a30526ade639bbca30
Leandro Silva <skriptkid@gmail.com>
2014-05-23 17:24:02 -0300

    Albert Melandre e Jose Luiz Neto

--- 2014/20140523 - Anagramas/Problem.php
+++ 2014/20140523 - Anagramas/Problem.php
@@ -10,7 +10,8 @@ class Anagrama {
 	}
 
 	public function get() {
-		$tmp = explode("", $this->string);
+		$tmp = str_split($this->string);
+
 		for ($i=0; $i < 5; $i++) {
 			/*for($j = 0; $j < 5; $j++){
 				if($i==$j){
