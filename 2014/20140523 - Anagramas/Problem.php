8fe0912b78c6ac7b1fe606dad85d8da979458c46
Leandro Silva <skriptkid@gmail.com>
2014-05-23 17:58:51 -0300

    Vitor e Aly

--- 2014/20140523 - Anagramas/Problem.php
+++ 2014/20140523 - Anagramas/Problem.php
@@ -3,6 +3,7 @@ class Anagrama {
 
 	private $string;
 	private $array;
+	private $index = 0;
 
 	function Anagrama($string){
 		$this->string = $string;
@@ -11,6 +12,15 @@ class Anagrama {
 
 	public function get() {
 		$tmp = str_split($this->string);
+		$this->inverso = array_reverse($tmp);
+		if($this->index < count($tmp)-1){
+			$vtemp = $tmp[$this->index+1];
+			$tmp[$this->index+1] = $tmp[$this->index];
+			$tmp[$this->index] = $vtemp;
+			$str = implode("", $tmp);
+			$this->array[] =  $str;
+			$this->get();
+		}
 
 		for ($i=0; $i < count($tmp); $i++) {
 			for($j = 0; $j < 5; $j++){
