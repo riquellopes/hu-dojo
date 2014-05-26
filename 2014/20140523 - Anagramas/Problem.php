82d2320b126e55953dfa006cd95b83c84dd21d79
Henrique Lopes <riquellopes@gmail.com>
2014-05-25 21:31:17 -0300

    Criando minha versão para solução do anagrama.

--- 2014/20140523 - Anagramas/Problem.php
+++ 2014/20140523 - Anagramas/Problem.php
@@ -1,32 +1,33 @@
 <?php
 class Anagrama {
-
 	private $string;
-	private $array;
-	private $index = 0;
-
-	function Anagrama($string){
+	private $result = array();
+	
+	public function __construct($string){
 		$this->string = $string;
-		$this->array = array();
-		$this->string_to_array = str_split($this->string);
-		$this->inverse = array_reverse($this->string_to_array);
+		$this->result = $this->processAnagrama($this->string);
 	}
-
-	public function get() {
-		if($this->index < count($this->string_to_array)-1) {
-			$vtemp = $this->string_to_array[$this->index+1];
-			$this->string_to_array[$this->index+1] = $this->string_to_array[$this->index];
-			$this->string_to_array[$this->index] = $vtemp;
-			$str = implode("", $this->string_to_array);
-			if($this->inverse != $str) {
-				$this->index++;
-				$this->array[] = $str;
-				$this->get();
+	
+	private function processAnagrama($string){
+		$result = array();
+		
+		if( !$string ){
+			$result[] = $string;
+		}else{	
+			foreach( str_split($string) as $s){
+				$p = preg_replace("/".$s."/", "", $string, 1);
+				
+				foreach( $this->processAnagrama($p) as $ana ){
+					$result[] = trim($s."".$ana);
+				}
+				
 			}
-		} else {
-			$this->index = 0;
-			$this->get();
 		}
-		return $this->array;
+		
+		return $result;
+	}
+	
+	public function get() {
+		return $this->result;
 	}
 }
--- 2014/20140523 - Anagramas/ProblemTest.php
+++ 2014/20140523 - Anagramas/ProblemTest.php
@@ -20,30 +20,21 @@ require dirname(__FILE__) . '/Problem.php';
 
 class ProblemTest extends PHPUnit_Framework_TestCase{
 
-  protected function setUp() {
+  function setUp() {
   	$this->anagrama = new Anagrama("baeta");
   }
-
-  function testSeRetornaString(){
-  	$this->assertInternalType(
-  		'array',
-  		$this->anagrama->get(),
-  		'Não é um array'
-  	);
+  
+  function test_para_a_palavra_baeta_deve_gerar_pelo_menos_o_anagrama_batae(){
+	$this->assertTrue(in_array('batae', $this->anagrama->get()));
+  }
+  
+  function test_para_a_palavra_biro_deve_existir_pelo_menos_anagranama_bior(){
+	 $ana = new Anagrama("biro");
+	 $this->assertTrue(in_array('bior', $ana->get()));
   }
 
-  function testSeRetornaBaeta() {
-  	// $this->assertContains('baeta', $this->anagrama->get(), 'Não retornou baeta');
-  	return "baeta";
+  function test_para_a_palavra_gato_deve_existir_pelo_menos_anagranama_gaot(){
+	 $ana = new Anagrama("gato");
+	 $this->assertTrue(in_array('gaot', $ana->get()));
   }
-
-   function testSeRetornaAbeta() {
-  	// $this->assertContains('abeta', $this->anagrama->get(), 'Não retornou abeta');
-  	return "abeta";
-  }
-
-  function testInAnagrama(){
-	$this->assertContains('aebta', $this->anagrama->get(), 'Não retornou aebta');  	
-  }
-
 }
